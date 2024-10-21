e1fed23b6f402f70e88de55368ae868853d1ac6f
Devin Frost <devin.frost4311@gmail.com>
2024-10-20 21:52:14 -0400

    glm qualifier requirement removed Changed the way uBuffer works

--- ent_camera.h
+++ ent_camera.h
@@ -1,5 +1,7 @@
+#pragma once
 #include "movable_ent.h"
 #include "uBuffer.h"
+#include "glm_master.h"
 
 class ent_camera: public ent_3d_2a
 {
@@ -7,8 +9,8 @@ class ent_camera: public ent_3d_2a
 		// constructor
 
 		ent_camera(
-			const glm::vec3& Position = { 0.0f,0.0f,0.0f }, const glm::vec3& Target = glm::vec3(1.0f, 0.0f, 0.0f),
-			const glm::vec3& Up_vector = { 0.0f,1.0f,0.0f }, const float& fov = 80.0f,
+			const vec3& Position = { 0.0f,0.0f,0.0f }, const vec3& Target = vec3(1.0f, 0.0f, 0.0f),
+			const vec3& Up_vector = { 0.0f,1.0f,0.0f }, const float& fov = 80.0f,
 			const float& aspect_ratio = 16.0f / 9.0f, const float& near_clip = 0.001f,
 			const float& far_clip = 1000.0f) :
 				fov(fov),
@@ -22,18 +24,27 @@ class ent_camera: public ent_3d_2a
 			target = Target;
 			up = Up_vector;
 		}
+		const char* get_name() const {
+			return "ent_camera";
+		}
 
 		// destructor
 		~ent_camera();
 		void update_view_matrix();
-		glm::mat4 get_view_matrix();
-		glm::mat4 get_perspective_matrix();
+		mat4 get_view_matrix();
+		mat4 get_perspective_matrix();
 		void update_UBO();
 		void bind_UBO(GLuint program); // might be nice to add a no-arg variant
 
+		void look_at_object(ent_3d_2a& target_object)
+		{
+			ent_3d_2a::look_at(target_object.get_position());
+		}
+//		void look_at_brush()
+
 		uBuffer<>& getUBO();
 	private:
-		glm::vec3 up;
+		vec3 up;
 		float fov;
 		float aspect_ratio;
 		float near_clip;
@@ -42,18 +53,22 @@ class ent_camera: public ent_3d_2a
 		
 };
 
+
 // When you aren't a lazy fuckface, change this to delete the ubo. Actually, it might free it, but still check
 ent_camera::~ent_camera() = default;
 
 
-glm::mat4 ent_camera::get_view_matrix() { return get_matrix(); }
-glm::mat4 ent_camera::get_perspective_matrix() { return glm::perspective(glm::radians(fov), aspect_ratio, near_clip, far_clip); }
+mat4 ent_camera::get_view_matrix() { return get_matrix(); }
+mat4 ent_camera::get_perspective_matrix() { return glm::perspective(glm::radians(fov), aspect_ratio, near_clip, far_clip); }
 void ent_camera::update_UBO()
 {
-	glm::mat4& view_matrix = glm::inverse(get_view_matrix());
-	glm::mat4& perspective_matrix = get_perspective_matrix();
-	ubo.updateData(0, sizeof(glm::mat4), glm::value_ptr(view_matrix));
-	ubo.updateData(sizeof(glm::mat4), sizeof(glm::mat4), glm::value_ptr(perspective_matrix));
+	mat4 view_matrix = glm::inverse(get_view_matrix());
+	mat4 perspective_matrix = get_perspective_matrix();
+	ubo.set(offsetof(gl_globals,view), view_matrix);
+	ubo.set(offsetof(gl_globals, projection), perspective_matrix);
+
+	//ubo.updateData(0, sizeof(mat4), glm::value_ptr(view_matrix));
+	//ubo.updateData(sizeof(mat4), sizeof(mat4), glm::value_ptr(perspective_matrix));
 }
 void ent_camera::bind_UBO(GLuint program)
 {
--- uBuffer.h
+++ uBuffer.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <GL/glew.h>
 #include <iostream>
+#include <cstddef>
+#include "globals.h"
 
-template <GLenum Usage = GL_STATIC_DRAW, GLsizeiptr Size = 132>
+
+template <GLenum Usage = GL_STATIC_DRAW, typename T = gl_globals>
 class uBuffer {
 public:
-    uBuffer() : bufferID(0), bufferSize(0) {}
+    uBuffer() : bufferID(0) {
+        std::memset(&data, 0, sizeof(T)); // Initialize to zero
+    }
 
     ~uBuffer() {
         if (bufferID != 0) {
@@ -13,13 +18,13 @@ public:
         }
     }
 
+    // Initialize the buffer
     void init() {
         if (bufferID != 0) {
             std::cerr << "Buffer already initialized." << std::endl;
             return;
         }
 
-        bufferSize = Size;
         glGenBuffers(1, &bufferID);
         if (bufferID == 0) {
             std::cerr << "Failed to generate buffer." << std::endl;
@@ -27,29 +32,55 @@ public:
         }
 
         glBindBuffer(GL_UNIFORM_BUFFER, bufferID);
-        glBufferData(GL_UNIFORM_BUFFER, bufferSize, nullptr, Usage);
+        glBufferData(GL_UNIFORM_BUFFER, sizeof(T), nullptr, Usage);
         glBindBuffer(GL_UNIFORM_BUFFER, 0);
     }
 
+    // Bind the buffer to a binding point
     void bindBase(GLuint bindingPoint) const {
         glBindBufferBase(GL_UNIFORM_BUFFER, bindingPoint, bufferID);
     }
 
-    void updateData(GLintptr offset, GLsizeiptr size, const void* data) const {
+    // Set a specific member using an offset
+    template <typename MemberType>
+    void set(size_t offset, const MemberType& newValue) {
+        // Check if the offset is valid
+        if (offset + sizeof(MemberType) > sizeof(T)) {
+            std::cerr << "Offset out of bounds." << std::endl;
+            return;
+        }
+
+        // Copy the new value to the specified offset
+        std::memcpy(reinterpret_cast<char*>(&data) + offset, &newValue, sizeof(MemberType));
+        update(); // Update the GPU buffer
+    }
+
+    // Get a specific member using an offset
+    template <typename MemberType>
+    MemberType get(size_t offset) {
+        // Check if the offset is valid
+        if (offset + sizeof(MemberType) > sizeof(T)) {
+            std::cerr << "Offset out of bounds." << std::endl;
+            return MemberType(); // Return default-constructed value
+        }
+
+        // Return the value at the specified offset
+        return *reinterpret_cast<MemberType*>(reinterpret_cast<char*>(&data) + offset);
+    }
+
+    // Update the buffer data on the GPU
+    void update() const {
         if (bufferID == 0) {
             std::cerr << "Buffer not initialized." << std::endl;
             return;
         }
-        //std::cout << "Uniform Buffer updated.\n";
 
         glBindBuffer(GL_UNIFORM_BUFFER, bufferID);
-        glBufferSubData(GL_UNIFORM_BUFFER, offset, size, data);
+        glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(T), &data);
         glBindBuffer(GL_UNIFORM_BUFFER, 0);
     }
 
-    GLuint getID() const { return bufferID; }
-
 private:
-    GLuint bufferID;
-    GLsizeiptr bufferSize;
+    GLuint bufferID; // OpenGL buffer ID
+    T data; // Local copy of data
 };
